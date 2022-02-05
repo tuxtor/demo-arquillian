79f940bab323450ca86b36a808d9266ff32af271
Víctor Orozco <me@vorozco.com>
2022-02-05 13:42:17 -0600

    Adding persistence support

---
+++ src/main/java/com/nabenik/model/Pet.java
@@ -0,0 +1,45 @@
+package com.nabenik.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "pet")
+public class Pet implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
